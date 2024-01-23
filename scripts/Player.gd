1b23057ec6f2ce521428e57a3f853caa07d787cc
Luana <luana@luana.dev.br>
2024-01-23 18:36:51 -0300

    water

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -4,13 +4,16 @@ extends CharacterBody3D
 @export var speed = 14
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
+@export var jump_impulse = 20
 
 var target_velocity = Vector3.ZERO
+var underwater = true
+
 
 func _physics_process(delta):
+	#Movement
 	# We create a local variable to store the input direction.
 	var direction = Vector3.ZERO
-
 	# We check for each move input and update the direction accordingly.
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
@@ -21,20 +24,33 @@ func _physics_process(delta):
 		# In 3D, the XZ plane is the ground plane.
 		direction.z += 1
 	if Input.is_action_pressed("move_forward"):
-		direction.z -= 1
-		
+		direction.z -= 1	
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		$Pivot.look_at(position + direction, Vector3.UP)
-
 	# Ground Velocity
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
-
 	# Vertical Velocity
 	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
-
+	# Jump
+	if is_on_floor() and Input.is_action_just_pressed("move_up	"):
+		target_velocity.y = jump_impulse
 	# Moving the Character
 	velocity = target_velocity
 	move_and_slide()
+	
+	# Is she underwater?
+	if position.y >= 0:
+		if not true: # check if inside manual water space
+			underwater = false
+		else:
+			underwater = true
+	if position.y <= 0:
+		if not true: # check if inside manual air space
+			underwater = true
+		else:
+			underwater = false
+	# testing
+	print('underwater ' + str(underwater))
